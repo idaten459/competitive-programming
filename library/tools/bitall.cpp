819629061dce456cef6e1c66fa7d742ef6df88df
idaten <shukai2013@gmail.com>
2019-05-09 01:57:14 +0900

    bit全探索ライブラリ

---
+++ library/tools/bitall.cpp
@@ -0,0 +1,18 @@
+void bitall(){
+    vector<int> a = {0,1,2,3};
+    ll n = a.size();
+    for(ll bit = 0; bit < (1<<n); ++bit){
+        vector<ll> s;
+        for(ll i = 0; i < n; ++i){
+            if(((bit>>i) & 1) == 1){
+                //処理部分
+                s.push_back(a[i]);
+            }
+        }
+        cout << "[ ";
+        for(int i=0;i<s.size();++i){
+            cout << s[i] << " ";
+        }
+        cout << "]\n";
+    }
+}
