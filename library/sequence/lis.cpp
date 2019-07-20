410b4f8ab14bf7fd0b6974dc8de3978f8dc37155
idaten_kyopro <shukai2013@gmail.com>
2019-07-21 00:28:13 +0900

    lisに広義と狭義の分岐を追加

--- library/sequence/lis.cpp
+++ library/sequence/lis.cpp
@@ -1,20 +1,30 @@
 /*
- LIS(最長増加部分列(の長さ
+ LIS(最長増加部分列)の長さ
  計算量はO(nlogn)
 */
 
-template<typename T,typename U>
-T lis(vector<U> a){
-    T rtn = 0;
+template<typename U>
+int lis(vector<U> a,bool broad=true){//broad=true:広義、false:狭義
+    int rtn = 0;
     vector<int> lis;
     lis.push_back(a[0]);
     FOR(i,1,a.size()){
-        if(lis.back()<=a[i]){
-            lis.push_back(a[i]);
+        if(broad){
+            if(lis.back() <=a [i]){
+                lis.push_back(a[i]);
+            }else{
+                auto itr = upper_bound(all(lis), a[i])-lis.begin();
+                lis[itr]=a[i];
+            }
         }else{
-            auto itr = upper_bound(all(lis), a[i])-lis.begin();
-            lis[itr]=a[i];
+            if (lis.back() < a[i]) {
+                lis.push_back(a[i]);
+            }else {
+                auto itr = lower_bound(all(lis), a[i]) - lis.begin();
+                lis[itr] = a[i];
+            }
         }
+        
     }
     rtn = lis.size();//LISの長さ
     return rtn;
