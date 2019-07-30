87d26306773ddb1662d5ebed9ab28165655d9ca8
Shukai Nakamura <shukai2013@gmail.com>
2019-07-30 09:21:34 +0900

    vectorの出力部分を修正

--- library/macro.cpp
+++ library/macro.cpp
@@ -13,19 +13,19 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int, int> P;
-template<typename T1,typename T2> inline bool chmin(T1 &a,T2 b){if(a>b){a=b;return 1;}return 0;}
-template<typename T1,typename T2> inline bool chmax(T1 &a,T2 b){if(a<b){a=b;return 1;}return 0;}
-template<class Type>struct is_vector : std::false_type{};
-template<class ValueType,class Alloc>struct is_vector<std::vector<ValueType,Alloc>> : std::true_type{};
+template<typename T1, typename T2> inline bool chmin(T1& a, T2 b) { if (a > b) { a = b; return 1; }return 0; }
+template<typename T1, typename T2> inline bool chmax(T1& a, T2 b) { if (a < b) { a = b; return 1; }return 0; }
+template<class Type>struct is_vector : std::false_type {};
+template<class ValueType, class Alloc>struct is_vector<std::vector<ValueType, Alloc>> : std::true_type {};
 template <typename T> inline ostream& operator << (ostream& out, const vector<T>& v) {
-    if (v.empty()) return out;
-    constexpr bool is_vector_v = is_vector<T>::value;
-    if(is_vector_v)for (auto itr = v.begin(); itr != v.end();)out << *itr << ((++itr!=v.end())?'\n':'\0');
-    else for (auto itr = v.begin(); itr != v.end();)out << *itr << ((++itr!=v.end())?' ':'\0');
-    return out;
+	if (v.empty())return out;
+	constexpr bool is_vector_v = is_vector<T>::value;
+	if (is_vector_v)for (auto itr = v.begin(); itr != v.end();)out << (*itr),out << ((++itr != v.end()) ? "\n" : "");
+	else for (auto itr = v.begin(); itr != v.end();)out << (*itr),out << ((++itr != v.end()) ? " " : "");
+	return out;
 }
-inline void put(){}
-template<class T> inline void put(const T& first){std::cout<<first;printf("\n");}
-template<class T, class... N> inline void put(const T& first, const N&... rest){std::cout<<first;printf(" ");put(rest...);}
-inline void putn(){}
-template<class T, class... N> inline void putn(const T& first, const N&... rest){std::cout<<first;printf("\n");putn(rest...);}
+inline void put() {}
+template<class T> inline void put(const T& first) { std::cout << first; printf("\n"); }
+template<class T, class... N> inline void put(const T& first, const N& ... rest) { std::cout << first; printf(" "); put(rest...); }
+inline void putn() {}
+template<class T, class... N> inline void putn(const T& first, const N& ... rest) { std::cout << first; printf("\n"); putn(rest...); }
