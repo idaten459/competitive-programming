e8f756ee5e22a129b062437363bebd966fa5aa24
idaten <shukai2013@gmail.com>
2019-10-18 15:38:53 +0900

    fix Bipartite

--- library/graph/bipartite-graph-judge.cpp
+++ library/graph/bipartite-graph-judge.cpp
@@ -3,53 +3,56 @@
  dfsがtrueを返したら、2部グラフ
  計算量は、O(m)?
  利用例は、
- https://atcoder.jp/contests/code-festival-2017-qualb/submissions/4893383
+ https://atcoder.jp/contests/code-festival-2017-qualb/submissions/8006141
 */
 
-struct edge{
-    int to;
+class Bipartite{
+public:
+    vector<vector<int>> path;
+    vector<int> res;
+    Bipartite(int n,vector<vector<int>> &path):path(path){
+        res.resize(n);
+        fill(all(res),-1);
+    }
+    
+    bool judge(){
+        return dfs(this->path,this->res,0,0);
+    }
+    bool dfs(vector<vector<int>>& path,vector<int>&res,int now,int color){
+        res[now]=color;
+        REP(i,path[now].size()){
+            int next=path[now][i];
+            if(res[next]==color){
+                return false;
+            }
+            if(res[next]==-1&&!dfs(path,res,next,1-color)){
+                return false;
+            }
+        }
+        return true;
+    }
 };
 
-int n,m;
-vector<vector<edge>> e;
-vector<int> c;
-
-bool dfs(int a,int color){
-    c[a]=color;
-    REP(i,e[a].size()){
-        if(c[e[a][i].to]==color){
-            return false;
-        }
-        if(c[e[a][i].to]==-1&&!dfs(e[a][i].to,1-color)){
-            return false;
-        }
-    }
-    return true;
-}
 int main(){
+    int n,m;
     cin >> n >> m;
     vector<int> a(m);
     vector<int> b(m);
-    e.resize(n);
+    vector<vector<int>> path(n);
     REP(i,m){
         cin >> a[i] >> b[i];
         a[i]--;
         b[i]--;
-        edge ed1;
-        ed1.to = b[i];
-        e[a[i]].push_back(ed1);
-        edge ed2;
-        ed2.to = a[i];
-        e[b[i]].push_back(ed2);
+        path[a[i]].push_back(b[i]);
+        path[b[i]].push_back(a[i]);
     }
-    c.resize(n);
-    fill(all(c),-1);
-    if(dfs(a[0],0)){
+    Bipartite bp(n, path);
+    if(bp.judge()){
         int cw=0,cb=0;
         REP(i,n){
-            if(c[i]==0){
+            if(bp.res[i]==0){
                 cw++;
-            }else if(c[i]==1){
+            }else if(bp.res[i]==1){
                 cb++;
             }
         }
