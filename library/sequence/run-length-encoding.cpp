231a158b1f5a50c6f7da1601dab1f7416559a715
idaten <shukai2013@gmail.com>
2019-11-04 12:49:56 +0900

    add source code

--- library/sequence/run-length-encoding.cpp
+++ library/sequence/run-length-encoding.cpp
@@ -9,3 +9,31 @@ run-length-enconding
 }
 という圧縮方法
 */
+
+template <typename T>
+vector<pair<int, T>> rle(vector<T> a)
+{
+    int n = (int)a.size();
+    vector<pair<int, T>> res;
+    if (n == 0)
+    {
+        return res;
+    }
+    T now = a[0];
+    int con = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        if (a[i] == now)
+        {
+            con++;
+        }
+        else
+        {
+            res.push_back(pair<int, T>(con, now));
+            now = a[i];
+            con = 1;
+        }
+    }
+    res.push_back(pair<int, T>(con, now));
+    return res;
+}
