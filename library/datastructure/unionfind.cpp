cb8e8bcf1358a4c4d625a032f9510d489bf8434d
Shukai Nakamura <shukai2013@gmail.com>
2019-12-04 22:30:59 +0900

    add funt uf

--- library/datastructure/unionfind.cpp
+++ library/datastructure/unionfind.cpp
@@ -1,41 +1,47 @@
-class UnionFind{
+class UnionFind {
 private:
-    vector<int> par,height,size;
+	vector<int> par, height, size;
+	int ele;
 public:
-    UnionFind(int n){
-        par.resize(n);
-        REP(i,n){
-            par[i] = i;
-        }
-        height.resize(n,0);
-        size.resize(n,1);
-    }
-    int root(int x){
-        if(par[x]==x){
-            return x;
-        }else{
-            par[x] = root(par[x]);
-            return par[x];
-        }
-    }
-    bool same(int x,int y){
-        return root(x)==root(y);
-    }
-    void unite(int x,int y){
-        x = root(x);
-        y = root(y);
-        if(x==y)return;
-        if(height[x]<height[y]){
-            par[x]=y;
-            size[y]+=size[x];
-        }else{
-            par[y]=x;
-            size[x]+=size[y];
-            if(height[x]==height[y])height[x]++;
-        }
-    }
-    int cnt(int x){
-        x=root(x);
-        return size[x];
-    }
+	UnionFind(int n) {
+		par.resize(n);
+		REP(i, n) {
+			par[i] = i;
+		}
+		height.resize(n, 0);
+		size.resize(n, 1);
+		ele = n;
+	}
+	int root(int x) {
+		if (par[x] == x) {
+			return x;
+		} else {
+			par[x] = root(par[x]);
+			return par[x];
+		}
+	}
+	bool same(int x, int y) {
+		return root(x) == root(y);
+	}
+	void unite(int x, int y) {
+		x = root(x);
+		y = root(y);
+		if (x == y)return;
+		ele--;
+		if (height[x] < height[y]) {
+			par[x] = y;
+			size[y] += size[x];
+		} else {
+			par[y] = x;
+			size[x] += size[y];
+			if (height[x] == height[y])height[x]++;
+		}
+	}
+	int get_size(int x) {
+		x = root(x);
+		return size[x];
+	}
+	int get_ele() {
+		return ele;
+	}
 };
