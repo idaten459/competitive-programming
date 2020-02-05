a49d8f40984e73fa8aa714149f12cf26d0752729
idaten <shukai2013@gmail.com>
2020-02-06 00:38:13 +0900

    point: bug fix

--- library/geometry/point.cpp
+++ library/geometry/point.cpp
@@ -1,64 +1,75 @@
-class point{
+/*
+二次元の座標もしくはベクトルを扱うクラス
+@verify https://atcoder.jp/contests/arc042/submissions/9916658
+*/
+
+template<typename T>class point{
 public:
-    double x,y;
-    point(double x,double y):x(x),y(y){}
+    T x,y;
+    point(T x,T y):x(x),y(y){}
     point():x(0),y(0){}
-    void set(double x,double y){
+    void set(T x,T y){
         this->x = x;
         this->y = y;
     }
-    double dist(point p){
-        double dx = x-p.x;
-        double dy = y-p.y;
-        double res = dx*dx+dy*dy;
-        res = sqrt(res);
-        return res;
+    T p2dist(point p){
+        T dx = x-p.x;
+        T dy = y-p.y;
+        return dx*dx+dy*dy;
     }
-    double dist(double x,double y){
+    T dist(point p){
+        return sqrt(p2dist(p));
+    }
+    T dist(T x,T y){
         return dist(point(x,y));
     }
-    
-    double inner_product(point p){
-        double res;
-        res = x*p.x+y*p.y;
-        return res;
+    T inner_product(point p){ // this・p
+        return x*p.x+y*p.y;
     }
-    double size(){
+    T outer_product(point p){ // this × p
+        return x*p.y-y*p.x;
+    }
+    T cos(point p1,point p2){ // 角度p1,this,p2の余弦
+        point t1 = p1-(*this),t2 = p2-(*this);
+        T ip = t1.inner_product(t2);
+        return ip/t1.size()/t2.size();
+    }
+    T sin(point p1,point p2){ // 角度p1,this,p2の正弦
+        point t1 = p1-(*this),t2 = p2-(*this);
+        T ip = t1.outer_product(t2);
+        return ip/t1.size()/t2.size();
+    }
+    T size(){
         return sqrt(x*x+y*y);
     }
     point norm(){
         return (*this)/size();
     }
-    point operator+(point p){
-        return point(x+p.x,y+p.y);
+    point project(point p1,point p2){ // p1,p2を通る直線へ下ろした垂線の足
+        point p3 = p2-p1;
+        T s = p1.p2dist(p2);
+        T t = (*this-p1)*p3/s;
+        point res = p3*t;
+        res+=p1;
+        return res;
     }
-    point operator-(point p){
-        return point(x-p.x,y-p.y);
+    T line_dist(point p1,point p2){ // p1,p2を通る直線との距離
+        point proj = this->project(p1, p2);
+        return this->dist(proj);
     }
-    point operator*(double t){
-        return point(x*t,y*t);
-    }
-    double operator*(point p){
-        return inner_product(p);
-    }
-    point operator/(double t){
-        return point(x/t,y/t);
-    }
-    void operator+=(point p){
-        x+=p.x;
-        y+=p.y;
-    }
-    void operator-=(point p){
-        x-=p.x;
-        y-=p.y;
-    }
-    void operator*=(double t){
-        x*=t;
-        y*=t;
-    }
+    point operator+(point p){return point(x+p.x,y+p.y);}
+    point operator-(point p){return point(x-p.x,y-p.y);}
+    point operator*(T t){return point(x*t,y*t);}
+    T operator*(point p){return inner_product(p);}
+    point operator/(T t){return point(x/t,y/t);}
+    void operator+=(point p){x+=p.x;y+=p.y;}
+    void operator-=(point p){x-=p.x;y-=p.y;}
+    void operator*=(T t){x*=t;y*=t;}
+    void operator/=(T t){x/=t;y/=t;}
 };
 
 int main(){
+    using point = point<double>
     int n;
     double px,py;
     cin >> px >> py;
