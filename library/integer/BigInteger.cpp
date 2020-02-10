604e61c7dff39399146035e2a6c3f048c596f492
Shukai Nakamura <shukai2013@gmail.com>
2020-02-10 16:22:48 +0900

    biginteger 途中まで

--- library/integer/BigInteger.cpp
+++ library/integer/BigInteger.cpp
@@ -1,242 +1,168 @@
-/*
- m進数の四則演算を行うクラス
- 順番が逆順なので注意
- 最大でm^(INT_MAX+1)-1まで扱える
- m>1,val>0であることが前提で動いている
- 四則演算一回あたりの計算量はO(val.size())
-*/
+#include <bits/stdc++.h>
+using namespace std;
 
-class BigInteger{
+class BigInteger {
 public:
-    int m;
-    vector<int> val;//進数の値を格納、n=sum(val[i]*m^i),val[0]=0とval.size()=0がどちらを0とするかが課題
-    BigInteger(int _m){//m進数,m>1,将来的にm<-1も対応するかもしれない
-        m=_m;
-    }
-    BigInteger(int _m,int _size){//m進数
-        m=_m;
-        val.resize(_size);
-    }
-    void zero_pad(vector<int>& a){//先頭の0を削除,値が0の時が1個だけ残す
-        while(a.size()>1&&a.back()==0){
-            a.pop_back();
-        }
-    }
-    BigInteger sum(BigInteger d){//m==d.mが前提
-        int max_len = max(val.size(),d.val.size());
-        BigInteger res(m,max_len);
-        int carry=0;
-        REP(i,max_len){
-            int temp=carry+oor(val.at(i))+oor(d.val.at(i));
-            carry=temp/m;
-            res.val[i]=temp%m;
-        }
-        if(carry!=0){
-            res.val.push_back(carry);
-        }
-        return res;
-    }
-    BigInteger dif(BigInteger d,bool turn_off_zero=false){//m==d.mかつval>d.valが前提,前頭の0を消したいときは第2引数をtrueに
-        int max_len = val.size();
-        BigInteger res(m,max_len);
-        int borrow=0;
-        REP(i,max_len){
-            int temp=oor(val.at(i))-oor(d.val.at(i))-borrow;
-            if(temp<0){
-                borrow=1;
-                temp+=m;
-            }else{
-                borrow=0;
-            }
-            res.val[i]=temp;
-        }
-        if(turn_off_zero){
-            zero_pad(res.val);
-        }
-        return res;
-    }
-    BigInteger sum(int d){//m>1が前提
-        int len = size();
-        BigInteger res(m,len);
-        int carry=0;
-        REP(i,len){
-            int temp=carry+oor(val.at(i))+(d%m);
-            d/=m;
-            carry=temp/m;
-            res.val[i]=temp%m;
-        }
-        if(carry!=0){
-            res.val.push_back(carry);
-        }
-        return res;
-    }
-    void sumeq(int d){//m>1が前提
-        int len = size();
-        int carry=0;
-        REP(i,len){
-            int temp=carry+oor(val.at(i))+(d%m);
-            d/=m;
-            carry=temp/m;
-            val[i]=temp%m;
-        }
-        if(carry!=0){
-            val.push_back(carry);
-        }
-        return ;
-    }
-    BigInteger dif(int d,bool turn_off_zero=false){//val>dが前提,m>1が前提
-        int len = val.size();
-        BigInteger res(m,len);
-        int borrow=0;
-        REP(i,len){
-            int temp=oor(val.at(i))-d%m-borrow;
-            d/=m;
-            if(temp<0){
-                borrow=1;
-                temp+=m;
-            }else{
-                borrow=0;
-            }
-            res.val[i]=temp;
-        }
-        if(turn_off_zero){
-            zero_pad(res.val);
-        }
-        return res;
-    }
-    void difeq(int d,bool turn_off_zero=false){//val>dが前提,m>1が前提
-        int len = val.size();
-        int borrow=0;
-        REP(i,len){
-            int temp=oor(val.at(i))-d%m-borrow;
-            d/=m;
-            if(temp<0){
-                borrow=1;
-                temp+=m;
-            }else{
-                borrow=0;
-            }
-            val[i]=temp;
-        }
-        if(turn_off_zero){
-            zero_pad(val);
-        }
-        return ;
-    }
-    BigInteger pro(int d){
-        int max_len = val.size();
-        BigInteger res(m,max_len);
-        int carry=0;
-        REP(i,max_len){
-            int temp=val[i]*d+carry;
-            carry=temp/m;
-            res.val[i]=temp%m;
-        }
-        while(carry!=0){
-            int temp=carry;
-            carry=temp/m;
-            res.val.push_back(temp%m);
-        }
-        //zero_pad(res.val);
-        return res;
-    }
-    BigInteger quo(int d){//d!=0が前提
-        int max_len = val.size();
-        BigInteger res(m,max_len);
-        int carry=0;
-        for(int i=max_len-1;i>=0;--i){
-            int temp=oor(val.at(i))+carry*m;
-            carry=temp%d;
-            res.val[i]=temp/d;
-        }
-        //zero_pad(res.val);
-        return res;
-    }
-    bool GREATER(BigInteger d){//m==d.mが前提
-        if(size()>d.size()){
-            return true;
-        }else if(size()<d.size()){
-            return false;
-        }
-        int s = size();
-        RREP(i,s){
-            if(val[i]>d.val[i]){
-                return true;
-            }else if(val[i]<d.val[i]){
-                return false;
-            }
-        }
-        return false;
-    }
-    bool GEQ(BigInteger d){//m==d.mが前提
-        if(size()>d.size()){
-            return true;
-        }else if(size()<d.size()){
-            return false;
-        }
-        int s = size();
-        RREP(i,s){
-            if(val[i]>d.val[i]){
-                return true;
-            }else if(val[i]<d.val[i]){
-                return false;
-            }
-        }
-        return true;
-    }
-    bool LESS(BigInteger d){//m==d.mが前提
-        if(size()>d.size()){
-            return true;
-        }else if(size()<d.size()){
-            return false;
-        }
-        int s = size();
-        RREP(i,s){
-            if(val[i]<d.val[i]){
-                return true;
-            }else if(val[i]>d.val[i]){
-                return false;
-            }
-        }
-        return false;
-    }
-    bool LEQ(BigInteger d){//m==d.mが前提
-        if(size()>d.size()){
-            return true;
-        }else if(size()<d.size()){
-            return false;
-        }
-        int s = size();
-        RREP(i,s){
-            if(val[i]<d.val[i]){
-                return true;
-            }else if(val[i]>d.val[i]){
-                return false;
-            }
-        }
-        return true;
-    }
-    bool EQUAL(BigInteger d){//m==d.mが前提
-        return GEQ(d)&&LEQ(d);
-    }
-    BigInteger operator+(BigInteger d){return sum(d);}
-    BigInteger operator-(BigInteger d){return dif(d);}
-    BigInteger operator+(int d){return sum(d);}
-    BigInteger operator-(int d){return dif(d);}
-    BigInteger operator*(int d){return pro(d);}
-    BigInteger operator/(int d){return quo(d);}
-    void operator+=(BigInteger d){val = sum(d).val;}
-    void operator-=(BigInteger d){val = dif(d).val;}
-    void operator+=(int d){sumeq(d);}
-    void operator-=(int d){difeq(d);}
-    void operator*=(int d){val = pro(d).val;}
-    void operator/=(int d){val = quo(d).val;}
-    bool operator>(BigInteger d){return GREATER(d);}
-    bool operator<(BigInteger d){return LESS(d);}
-    bool operator>=(BigInteger d){return GEQ(d);}
-    bool operator<=(BigInteger d){return LEQ(d);}
-    bool operator==(BigInteger d){return EQUAL(d);}
-    int size(){
-        return val.size();
-    }
+	using ll = int64_t;
+	vector<int> val; // 10^9ごとに保管
+	int sign = 0; // 1のとき負
+	const ll u = 1e9;
+	BigInteger(vector<int>& a) :val(a) {}
+	BigInteger(int a) {
+		val.clear();
+		val.push_back(a);
+	}
+	/*BigInteger(const string&& s) {
+	}*/
+	BigInteger(const string& s) {
+		ll tmp = 0;
+		int l = (int)s.size();
+		int sh = 0;
+		if (s[0] == '-') {
+			sign = 1;
+			sh = 1;
+		}
+		for (int i = sh; i < l; ++i) {
+			tmp *= 10;
+			tmp += (ll)s[i] - '0';
+			if ((l - 1 - i) % 9 == 0) {
+				val.push_back(tmp);
+				tmp = 0;
+			}
+		}
+		reverse(val.begin(), val.end());
+		supplies();
+	}
+	BigInteger() {}
+	void supplies() {
+		int l = val.size();
+		for (int i = l - 1; i > 0; --i) {
+			if (val[i] == 0) {
+				val.pop_back();
+			} else {
+				break;
+			}
+		}
+	}
+	BigInteger inv() {
+		this->sign = !this->sign;
+		return *this;
+	}
+	ll get(int index) {
+		if (val.size() < index) {
+			return val[index];
+		} else {
+			return 0;
+		}
+	}
+	BigInteger add(BigInteger a) { // this + a
+		if (this->sign != a.sign) {
+			a.sign = !a.sign;
+			return sub(a.inv());
+		}
+		this->supplies();
+		a.supplies();
+		BigInteger res;
+		int m = max(this->val.size(), a.val.size());
+		res.val.resize(m);
+		int carry = 0;
+		for (int i = 0; i < m; ++i) {
+			res.val[i] = this->val[i] + a.val[i] + carry;
+			carry = res.val[i] / u; // 此処の演算は2冪の方が速い
+		}
+		if (carry > 0) {
+			res.val.push_back(carry);
+		}
+		res.sign = this->sign;
+		return res;
+	}
+	BigInteger sub(BigInteger a) { // this - a
+		if (this->sign == a.sign) {
+			a.sign = !a.sign;
+			return sub(a.inv());
+		}
+		this->supplies();
+		a.supplies();
+		int m = max(this->val.size(), a.val.size());
+		BigInteger res;
+		res.sign = this -> sign;
+		res.val.resize(m);
+		for (int i = m-1; i>= 0; --i) {
+			
+		}
+		res.supplies();
+		return res;
+	}
+	int size() {
+		this->supplies();
+		return this->val.size();
+	}
+	bool unsinged_greater(BigInteger a) { // 符号を無視したときのa>b
+		//this->supplies();
+		//a.supplies();
+		if (this->size() > a.size()) {
+			return true;
+		}
+		if (this->size() < a.size()) {
+			return false;
+		}
+		int s = size();
+		for (int i = s - 1; i >= 0; --i) {
+			if (val[i] > a.val[i]) {
+				return true;
+			} else if (val[i] < a.val[i]) {
+				return false;
+			}
+		}
+		return false;
+	}
+	bool signed_greater(BigInteger a) { // 符号を考慮したときのa>b
+		if (this->sign) {
+			if (a.sign) {
+
+			} else {
+				return false; // -0の時やばいいい
+			}
+		} else {
+			if (a.sign) {
+				return true;
+			} else {
+
+			}
+		}
+	}
+	string to_string() { // 此処の処理は10冪の方が圧倒的に楽
+		string res = "";
+		stringstream ss;
+		//bool first_zero = true;
+		int first_zero = 0;
+		for (int i = 0; i < val.size(); ++i) {
+			if (first_zero==0) {
+				if (val[i] != 0) {
+					first_zero = 1;
+					ss << val[i];
+				}
+			} else {
+				ss << std::setw(9) << std::setfill('0') << val[i];
+			}
+		}
+		if (first_zero == 0) {
+			ss << '0';
+		}
+		
+		return ss.str();
+	}
+	ll operator[](int a) { return get(a); }
 };
+
+template<typename T> void put(T a) {
+	cout << a << endl;
+}
+
+int main() {
+	string s = "123456789012345657890";
+	BigInteger b1(s);
+	//b1 = b1.add("123");
+	cout << b1.to_string() << endl;
+	return 0;
+}
