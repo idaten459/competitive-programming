ddb2f055dcfcc99b26818eaf4c7a7bfd6f5f1199
Shukai Nakamura <shukai2013@gmail.com>
2020-02-11 23:28:53 +0900

    rename

--- library/convenient/min_natural.cpp
+++ library/convenient/min_natural.cpp
@@ -1,5 +1,5 @@
 // 単位元を-1とする自然数のmin関数
-template<typename T> inline T min(T a, T b) {
+template<typename T> inline T min_n(T a, T b) {
 	if (a == -1) return b;
 	if (b == -1) return a;
 	if (a > b) {
