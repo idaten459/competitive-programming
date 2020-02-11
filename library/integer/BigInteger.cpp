693e4ba46c7c830f31fb191d4598f735d22e7c44
Shukai Nakamura <shukai2013@gmail.com>
2020-02-11 23:28:02 +0900

    add mul

--- library/integer/BigInteger.cpp
+++ library/integer/BigInteger.cpp
@@ -1,25 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
+/*
+多倍長整数のライブラリ
+計算量は桁数をLとして加減算がO(L),乗算がO(L^2)
+FFTを用いて乗算をO(L*log L)になるらしいが未実装(そもそもFFT自体がまだ)
+*/
 
 class BigInteger {
 public:
 	using ll = int64_t;
-	vector<int> val; // 10^9ごとに保管
-	int sign = 0; // 1のとき負
+	vector<ll> val; // 10^9ごとに保管
+	int neg = 0; // 1のとき負
 	const ll u = 1e9;
-	BigInteger(vector<int>& a) :val(a) {}
+	BigInteger(vector<ll>& a) :val(a) {}
 	BigInteger(int a) {
 		val.clear();
 		val.push_back(a);
 	}
-	/*BigInteger(const string&& s) {
-	}*/
 	BigInteger(const string& s) {
 		ll tmp = 0;
 		int l = (int)s.size();
 		int sh = 0;
 		if (s[0] == '-') {
-			sign = 1;
+			neg = 1;
 			sh = 1;
 		}
 		for (int i = sh; i < l; ++i) {
@@ -33,7 +34,9 @@ public:
 		reverse(val.begin(), val.end());
 		supplies();
 	}
-	BigInteger() {}
+	BigInteger() {
+		val.push_back(0);
+	}
 	void supplies() {
 		int l = val.size();
 		for (int i = l - 1; i > 0; --i) {
@@ -44,53 +47,91 @@ public:
 			}
 		}
 	}
-	BigInteger inv() {
-		this->sign = !this->sign;
-		return *this;
-	}
+	
 	ll get(int index) {
-		if (val.size() < index) {
+		if (size() > index) {
 			return val[index];
 		} else {
 			return 0;
 		}
 	}
 	BigInteger add(BigInteger a) { // this + a
-		if (this->sign != a.sign) {
-			a.sign = !a.sign;
-			return sub(a.inv());
+		if (this->neg != a.neg) {
+			a.neg = !a.neg;
+			return sub(a);
 		}
-		this->supplies();
-		a.supplies();
 		BigInteger res;
-		int m = max(this->val.size(), a.val.size());
+		int m = max(this->size(), a.size());
 		res.val.resize(m);
 		int carry = 0;
 		for (int i = 0; i < m; ++i) {
-			res.val[i] = this->val[i] + a.val[i] + carry;
+			res.val[i] = (*this)[i] + a[i] + carry;
 			carry = res.val[i] / u; // 此処の演算は2冪の方が速い
+			res.val[i] %= u;
 		}
 		if (carry > 0) {
 			res.val.push_back(carry);
 		}
-		res.sign = this->sign;
+		res.neg = this->neg;
+		res.neg_zero();
 		return res;
 	}
 	BigInteger sub(BigInteger a) { // this - a
-		if (this->sign == a.sign) {
-			a.sign = !a.sign;
-			return sub(a.inv());
+		if (this->neg != a.neg) {
+			a.neg = !a.neg;
+			return add(a);
 		}
-		this->supplies();
-		a.supplies();
-		int m = max(this->val.size(), a.val.size());
+		int m = max(this->size(), a.size());
 		BigInteger res;
-		res.sign = this -> sign;
+		res.neg = this->neg;
 		res.val.resize(m);
-		for (int i = m-1; i>= 0; --i) {
-			
+		int borrow = 0;
+		if (unsinged_greater(a)) {
+			for (int i = 0; i < m; ++i) {
+				res.val[i] = (*this)[i] - a[i] - borrow;
+				borrow = 0;
+				if (res.val[i] < 0) {
+					borrow = (std::abs(res.val[i]) + u - 1) / u;
+					res.val[i] += borrow * u;
+				}
+			}
+		} else {
+			res.neg = !res.neg;
+			for (int i = 0; i < m; ++i) {
+				res.val[i] = a[i] - (*this)[i] - borrow;
+				borrow = 0;
+				if (res.val[i] < 0) {
+					borrow = (std::abs(res.val[i]) + u - 1) / u;
+					res.val[i] += borrow * u;
+				}
+			}
 		}
 		res.supplies();
+		res.neg_zero();
+		return res;
+	}
+	BigInteger mul(BigInteger a) { // this * a
+		BigInteger res;
+		res.neg = this->neg ^ a.neg;
+		int m = this->size() + a.size();
+		res.val.resize(m + 2,0);
+		vector<ll> tmp(this->size() + 1);
+		ll carry = 0;
+		for (int j = 0; j < a.size(); ++j) {
+			for (int i = 0; i < this->size(); ++i) {
+				ll t = (*this)[i] * a[j];
+				t += carry;
+				res.val[i + j] += t;
+				carry = res.val[i + j] / u;
+				res.val[i + j] %= u;
+				//tmp[i + j];
+			}
+			res.val[j + this->size()] += carry;
+			carry = 0;
+		}
+		res.val[a.size() + this->size()] += carry;
+		res.supplies();
+		res.neg_zero();
 		return res;
 	}
 	int size() {
@@ -98,71 +139,119 @@ public:
 		return this->val.size();
 	}
 	bool unsinged_greater(BigInteger a) { // 符号を無視したときのa>b
-		//this->supplies();
-		//a.supplies();
 		if (this->size() > a.size()) {
 			return true;
 		}
 		if (this->size() < a.size()) {
 			return false;
 		}
-		int s = size();
+		int s = this->size();
 		for (int i = s - 1; i >= 0; --i) {
-			if (val[i] > a.val[i]) {
+			if ((*this)[i] > a[i]) {
 				return true;
-			} else if (val[i] < a.val[i]) {
+			} else if ((*this)[i] < a[i]) {
 				return false;
 			}
 		}
 		return false;
 	}
-	bool signed_greater(BigInteger a) { // 符号を考慮したときのa>b
-		if (this->sign) {
-			if (a.sign) {
-
-			} else {
-				return false; // -0の時やばいいい
-			}
-		} else {
-			if (a.sign) {
-				return true;
-			} else {
-
+	bool is_zero() {
+		if (this->size() == 1 && val[0] == 0) {
+			return true;
+		}
+		return false;
+	}
+	void neg_zero() {
+		if (this->is_zero() && this->neg) {
+			this->neg = 0;
+		}
+	}
+	bool signed_equal(BigInteger a) {
+		if (this->neg != a.neg) {
+			return false;
+		}
+		if (this->val.size() != a.size()) {
+			return false;
+		}
+		for (int i = 0; i < this->val.size(); ++i) {
+			if (this->val[i] != a.val[i]) {
+				return false;
 			}
 		}
+		return true;
+	}
+	bool signed_greater(BigInteger a) { // 符号を考慮したときのa>b
+		this->neg_zero();
+		a.neg_zero();
+		if (this->neg) {
+			if (a.neg) {
+				return (!unsinged_greater(a)) || signed_equal(a);
+			} else {
+				return false;
+			}
+		} else {
+			if (a.neg) {
+				return true;
+			} else {
+				return unsinged_greater(a);
+			}
+		}
+		return false;
 	}
 	string to_string() { // 此処の処理は10冪の方が圧倒的に楽
 		string res = "";
 		stringstream ss;
-		//bool first_zero = true;
-		int first_zero = 0;
-		for (int i = 0; i < val.size(); ++i) {
-			if (first_zero==0) {
-				if (val[i] != 0) {
-					first_zero = 1;
-					ss << val[i];
-				}
+		this->neg_zero();
+		if (this->neg) {
+			ss << '-';
+		}
+		int l = val.size();
+		for (int i = l - 1; i >= 0; --i) {
+			if (i == l - 1) {
+				ss << val[i];
 			} else {
 				ss << std::setw(9) << std::setfill('0') << val[i];
 			}
 		}
-		if (first_zero == 0) {
-			ss << '0';
-		}
-		
 		return ss.str();
 	}
 	ll operator[](int a) { return get(a); }
+	BigInteger operator+(BigInteger a) { return this->add(a); }
+	BigInteger operator-(BigInteger a) { return this->sub(a); }
+	BigInteger operator*(BigInteger a) { return this->mul(a); }
+	void operator+=(BigInteger a) {
+		BigInteger res = this->add(a);
+		this->neg = res.neg;
+		this->val = res.val;
+	}
+	void operator-=(BigInteger a) {
+		BigInteger res = this->sub(a);
+		this->neg = res.neg;
+		this->val = res.val;
+	}
+	void operator*=(BigInteger a) {
+		BigInteger res = this->mul(a);
+		this->neg = res.neg;
+		this->val = res.val;
+	}
 };
 
 template<typename T> void put(T a) {
 	cout << a << endl;
 }
 
+string solve(string a, string b) {
+	BigInteger b1(a), b2(b);
+	b1 -= b2;
+	return b1.to_string();
+}
+
 int main() {
-	string s = "123456789012345657890";
-	BigInteger b1(s);
-	//b1 = b1.add("123");
+	string a, b;
+	cin >> a >> b;
+	BigInteger b1(a), b2(b);
+	b1 *= b2;
 	cout << b1.to_string() << endl;
+	
 	return 0;
 }
