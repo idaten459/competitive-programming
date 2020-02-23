8ea8ce59e9e84e509e55c29d4483a269a0abd428
Shukai Nakamura <shukai2013@gmail.com>
2020-02-23 20:36:06 +0900

    implement : sqrInt

---
+++ library/integer/sqrt.cpp
@@ -0,0 +1,32 @@
+/*
+n>=0が前提
+flag=true, √n以下の最大の整数
+flag=false,√n以上の最小の整数
+*/
+
+template<typename T>
+T sqrtInt(T n, bool flag) {
+	T ok = 0, ng = 0;
+	if (flag) {
+		ng = n;
+		while (abs<T>(ng - ok) > 1) {
+			T mid = (ng + ok) / 2;
+			if (mid * mid > n) {
+				ng = mid;
+			} else {
+				ok = mid;
+			}
+		}
+	} else {
+		ok = n;
+		while (abs<T>(ok - ng) > 1) {
+			T mid = (ng + ok) / 2;
+			if (mid * mid < n) {
+				ng = mid;
+			} else {
+				ok = mid;
+			}
+		}
+	}
+	return ok;
+}
